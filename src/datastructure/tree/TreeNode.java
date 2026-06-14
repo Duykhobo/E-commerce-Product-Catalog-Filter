b12413a68b6f50bc3001946460860d8fc8016ebc
tquan3007 <thienquan.30072006@gmail.com>
2026-06-14 09:45:39 +0700

    feat: đã xong chèn sản phẩm vào bst tree

--- src/datastructure/tree/TreeNode.java
+++ src/datastructure/tree/TreeNode.java
@@ -1,13 +1,39 @@
 package datastructure.tree;
 
 public class TreeNode<T> {
-    public T data;
-    public TreeNode<T> left;
-    public TreeNode<T> right;
+    private T data;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
 
     public TreeNode(T data) {
         this.data = data;
         this.left = null;
         this.right = null;
     }
+
+    public T getData() {
+        return data;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setLeft(TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode<T> right) {
+        this.right = right;
+    }
+
+    
 }
